5dcbfb625cabf1d6990c70ecf3ecfbc63d5f30dd
KaylaDoesStuff <162106880+KaylaDoesStuff@users.noreply.github.com>
2024-06-04 17:52:35 -0700

    colon

--- src/distro.py
+++ src/distro.py
@@ -4,6 +4,6 @@ class Arch():
     def __init__(self, package):
         self.package = package
 
-    def install(self)
+    def install(self):
         command = "sudo pacman -S"+self
         os.system(command)
